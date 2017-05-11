d0f99df4fcf51c8be8d3b103d0a88ea233f28903
Artem Tihonov <iamtihonov@gmail.com>
2017-05-11 09:35:56 +0300

    Добавил вывод сообщения в лог

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,21 @@
+#include <android/log.h>
 #include <jni.h>
+#include <string>
 
-extern "C"
+extern "C"//При експорте функций имена их не будут изменятся
 JNIEXPORT jintArray JNICALL
-Java_ru_artem_ndktest2_MainActivity_getTestIntArray(JNIEnv *env, jobject /* this */, char *request) {
+Java_ru_artem_ndktest2_MainActivity_getTestIntArray(
+        JNIEnv *env, //указатель на объект, представляющий из себя JNI-окружение
+        jobject, //класс, которому принадлежит объявление нативного метода в Java. Фактически это this
+        char *request) {//входящая строка
+
+    std::string tag("test");
+    std::string message("Hello from C++!");
+    __android_log_print(ANDROID_LOG_INFO, tag.c_str(), "%s", message.c_str());//вывод сообщения в лог
+
     jint a[] = {1, 2, 3, 4, 5, 6};
     jintArray ret = env->NewIntArray(6);
     env->SetIntArrayRegion(ret, 0, 6, a);
+
     return ret;
 }
