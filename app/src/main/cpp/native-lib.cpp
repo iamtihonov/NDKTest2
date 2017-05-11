acc4b595f45dc0ba59a16ee14fedb96a5302ea7e
Artem Tihonov <iamtihonov@gmail.com>
2017-05-11 08:27:58 +0300

    add return array

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,12 +1,10 @@
 #include <jni.h>
-#include <string>
-#include <iostream>
 
 extern "C"
-JNIEXPORT jstring JNICALL
-Java_ru_artem_ndktest2_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++ 1!";
-    return env->NewStringUTF(hello.c_str());
+JNIEXPORT jintArray JNICALL
+Java_ru_artem_ndktest2_MainActivity_getTestIntArray(JNIEnv *env, jobject /* this */) {
+    jint a[] = {1, 2, 3, 4, 5, 6};
+    jintArray ret = env->NewIntArray(6);
+    env->SetIntArrayRegion(ret, 0, 6, a);
+    return ret;
 }
--- app/src/main/java/ru/artem/ndktest2/MainActivity.java
+++ app/src/main/java/ru/artem/ndktest2/MainActivity.java
@@ -2,6 +2,7 @@ package ru.artem.ndktest2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,12 +19,12 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        int [] result = getTestIntArray();
+        tv.setText("size = " + result.length);
+        for(int i = 0; i < result.length; i++) {
+            Log.d("test", String.valueOf(result[i]));
+        }
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    public native int[] getTestIntArray();
 }
